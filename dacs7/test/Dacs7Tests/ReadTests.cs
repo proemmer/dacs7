b23681861567ea56a2977b2d1e7d9df0175caeed
Benjamin Prömmer <benjamin.proemmer@outlook.com>
2018-04-16 16:33:24 +0200

    added tests

--- dacs7/test/Dacs7Tests/ReadTests.cs
+++ dacs7/test/Dacs7Tests/ReadTests.cs
@@ -1,4 +1,9 @@
-﻿using Dacs7;
+﻿
+//#define REALPLC
+
+#if REALPLC
+
+using Dacs7;
 using System;
 using System.Linq;
 using System.Threading.Tasks;
@@ -12,6 +17,32 @@ namespace Dacs7Tests
 
 
         [Fact]
+        public async Task ReadMultibleData()
+        {
+            var client = new Dacs7Client(Address);
+            try
+            {
+                await client.ConnectAsync();
+
+                var results = (await client.ReadAsync(ReadItemSpecification.Create<byte[]>("DB1114", 0, 1000),
+                                                       ReadItemSpecification.Create<byte[]>("DB1114", 2200, 100),
+                                                       ReadItemSpecification.Create<byte[]>("DB1114", 1000, 1000),
+                                                       ReadItemSpecification.Create<byte[]>("DB1114", 200, 100))).ToArray();
+
+
+                Assert.Equal(4, results.Count());
+                Assert.Equal(1000, results[0].Data.Length);
+                Assert.Equal(100, results[1].Data.Length);
+                Assert.Equal(1000, results[2].Data.Length);
+                Assert.Equal(100, results[3].Data.Length);
+            }
+            finally
+            {
+                await client.DisconnectAsync();
+            }
+        }
+
+        [Fact]
         public async Task ReadWriteBigDBData()
         {
             var client = new Dacs7Client(Address);
@@ -22,7 +53,6 @@ namespace Dacs7Tests
                 var results1 = await client.WriteAsync(WriteItemSpecification.Create("DB1114", 0, results0));
                 var results2 = (await client.ReadAsync(ReadItemSpecification.Create<byte[]>("DB1114", 0, 1000)));
                 Assert.True(results0.Span.SequenceEqual(results2.FirstOrDefault().Data.Span));
-
             }
             finally
             {
@@ -31,3 +61,5 @@ namespace Dacs7Tests
         }
     }
 }
+
+#endif
