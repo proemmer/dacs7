6225d360cdbc9ed951928d5da2dfe9725dd6ddf6
Benjamin Prömmer <benjamin.proemmer@outlook.com>
2019-02-05 10:53:42 +0100

    Added SpecializedLinqExtensions

---
+++ dacs7/src/Dacs7/Helper/SpecializedLinqExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dacs7.Helper
+{
+    internal static class SpecializedLinqExtensions
+    {
+        public static bool Any<TSource>(this IList<TSource> source)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+            return source.Count > 0;
+        }
+
+        public static bool Any<TSourceKey, TSourceValue>(this IDictionary<TSourceKey, TSourceValue> source)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+            return source.Count > 0;
+        }
+    }
+}
