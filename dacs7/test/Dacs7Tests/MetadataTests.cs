12db4ed7d612caa7f58ba9e657d33710984a0b06
Benjamin Prömmer <benjamin.proemmer@outlook.com>
2020-06-24 08:40:54 +0200

    changed metadata, ich block does not exists, an empty metadata structure will be received, and no exception will be thrown, so you can also check the block availability with this command

---
+++ dacs7/test/Dacs7Tests/MetadataTests.cs
@@ -0,0 +1,23 @@
+﻿using Dacs7.Metadata;
+using Dacs7Tests.ServerHelper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Dacs7.Tests
+{
+    public class MetadataTests
+    {
+        [Fact]
+        public async Task ReadMetadataOfNotExistingBlock()
+        {
+            await PlcTestServer.ExecuteClientAsync(async (client) =>
+            {
+                var x = await client.ReadBlockInfoAsync(PlcBlockType.Db, 66);
+                Assert.Equal(0, x.CodeSize);
+            });
+        }
+    }
+}
